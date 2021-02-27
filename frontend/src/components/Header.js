faee2853be02d00d8bfc77b6db1c0596b4a622d1
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-02-27 18:43:33 -0500

    Añadiendo el icono de la página

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import brayan from '../img/brayan.jpg';
-import jacobo from '../img/jacobo.jpg';
+import Brayan from '../img/brayan.jpg';
+import Jacobo from '../img/jacobo.jpg';
 
 const Header = () => {
     return (
         <header>
             <span className="logo">To-Do List</span>
             <div>
-                <a target="blank" href="https://github.com/brayangomez22"><img src={brayan} className="photo"></img></a>
-                <a target="blank" href="https://github.com/JacoboGarcesO"><img src={jacobo} className="photo"></img></a>
+                <a target="blank" href="https://github.com/brayangomez22"><img src={Brayan} alt="img_Jacobo" className="photo"></img></a>
+                <a target="blank" href="https://github.com/JacoboGarcesO"><img src={Jacobo} alt="img_Brayan" className="photo"></img></a>
             </div>
             <nav className="menu">
                 <a target="blank" href="https://github.com/JacoboGarcesO/KATA_REFACTORIZANDO_CRUD">Repositorio</a>

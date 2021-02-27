98c6676876e1d4e1de01161584306b8d7343fbcc
Brayan <gomezmancobrayanalexander@gmail.com>
2021-02-27 18:26:39 -0500

    Ultimas modificaciones del diseño

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
-
+import brayan from '../img/brayan.jpg';
+import jacobo from '../img/jacobo.jpg';
 
 const Header = () => {
     return (
         <header>
             <span className="logo">To-Do List</span>
+            <div>
+                <a target="blank" href="https://github.com/brayangomez22"><img src={brayan} className="photo"></img></a>
+                <a target="blank" href="https://github.com/JacoboGarcesO"><img src={jacobo} className="photo"></img></a>
+            </div>
             <nav className="menu">
                 <a target="blank" href="https://github.com/JacoboGarcesO/KATA_REFACTORIZANDO_CRUD">Repositorio</a>
             </nav>
